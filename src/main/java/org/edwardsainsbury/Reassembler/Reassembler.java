7a3936557d189ecbb49c8120446a04050389084c
edwardsainsbury <runrun93>
2018-06-09 12:19:45 +0100

    Working solution refactor

--- src/main/java/org/edwardsainsbury/Reassembler/Reassembler.java
+++ src/main/java/org/edwardsainsbury/Reassembler/Reassembler.java
@@ -1,11 +1,14 @@
 package org.edwardsainsbury.Reassembler;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 
 public class Reassembler {
-    private ArrayList<char[]> fragments;
+
     private String reassembled;
 
     public Reassembler(String inputString){
@@ -15,56 +18,110 @@ public class Reassembler {
         for (String fragment : fragmentArray){
             fragments.add(fragment.toCharArray());
         }
-        this.fragments = fragments;
-        this.reassembled = reassemble();
+
+        this.reassembled = reassemble(fragments);
     }
 
-    private int getOverlap(char left[], char right[] ) {
-        for (int i = 0; i < left.length; i++) {
-            char[] arr1 = Arrays.copyOfRange(left, left.length - i - 1, left.length);
-            char[] arr2 = Arrays.copyOfRange(right, 0, i + 1);
+    public static void main(String[] args) {
 
-            if (Arrays.equals(arr1, arr2) && arr1.length != left.length) {
-                return i+1;
+        try (BufferedReader in = new BufferedReader(new FileReader(args[0]))) {
+            String fragmentProblem;
+            while ((fragmentProblem = in.readLine()) != null) {
+                Reassembler reassembler = new Reassembler(fragmentProblem);
+                System.out.println(reassembler.getReassembled());
             }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Error: Please provide path to file");
+        } catch (FileNotFoundException e) {
+            System.out.println("Error: File not found.");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return 0;
+
     }
 
-
+    /**
+     * concat:
+     * Utility method to concatenate a second array to the first
+     *
+     * @param first  - char[] Char array to be appended to
+     * @param second - char[] Char array to append first array
+     * @return result - char[] Concatented result
+     */
     private static char[] concat(char[] first, char[] second) {
         char[] result = Arrays.copyOf(first, first.length + second.length);
         System.arraycopy(second, 0, result, first.length, second.length);
         return result;
     }
 
-    private String reassemble(){
-        // Sort fragments to add larger first
+    public String getReassembled() {
+        return reassembled;
+    }
+
+    /**
+     * getOverlaps:
+     * Method that finds left and right hand out overlaps between a base fragment and a test fragment. Inner overlaps.
+     * where the whole test fragment is a substring of the base fragment, are ignored. This limits interations to
+     * a maximum of (shortest length * 2)
+     *
+     * @param testFragment - char[] to compare either side of base char[]
+     * @param baseFragment - char[] to be used as a base for comparison
+     * @return overlaps - int array containing the left and right hand overlaps
+     */
+    private int getOverlaps(char testFragment[], char baseFragment[]) {
+        int[] overlaps = new int[2];
+        char[][] fragments = {testFragment, baseFragment};
+        for (int j = 0; j < 2; j++) {
+            char[] fragment = fragments[j];
+            char[] base = fragments[(j + 1) % 2];
+            int longest = Math.min(base.length, fragment.length);
+            for (int i = 0; i < longest; i++) {
+                char[] arr1 = Arrays.copyOfRange(fragment, fragment.length - i - 1, fragment.length);
+                char[] arr2 = Arrays.copyOfRange(base, 0, i + 1);
+                if (Arrays.equals(arr1, arr2) && arr1.length != fragment.length) {
+                    overlaps[j] = i + 1;
+                    break;
+                }
+            }
+        }
+        if (overlaps[0] > overlaps[1]) {
+            return overlaps[0] * -1;
+        }
+
+        return overlaps[1];
+    }
+
+    /**
+     * @return
+     */
+    private String reassemble(ArrayList<char[]> fragments) {
+        /* Sort fragments to add largest first */
         fragments.sort(Comparator.comparingInt(a -> -a.length));
 
         char[] base = fragments.get(0);
         fragments.remove(0);
-        int noAdd = 0;
+        int potentialSubstrings = 0;
 
-        // Loop while there are remaining fragemnts
-        while (noAdd != fragments.size()) {
+        /* Loop while until potentialSubstrings == remaining fragments */
+        while (potentialSubstrings != fragments.size()) {
             int bestOverlap = 0;
             int bestOverlapFragmentIndex = 0;
-            String bestOverlapDirection = "false";
+            String bestOverlapDirection = "left";
 
             for (int i = 0; i < fragments.size(); i++) {
                 char[] test = fragments.get(i);
-                int leftOverlap = getOverlap(test, base);
-                int rightOverlap = getOverlap(base, test);
-                if (leftOverlap > bestOverlap && leftOverlap > rightOverlap) {
-                    bestOverlap = leftOverlap;
-                    bestOverlapFragmentIndex = i;
-                    bestOverlapDirection = "left";
-                } else if (rightOverlap > bestOverlap) {
-                    bestOverlap = rightOverlap;
-                    bestOverlapFragmentIndex = i;
+                int overlap = getOverlaps(test, base);
+                int newBestOverlap = Math.abs(overlap);
+
+                if (newBestOverlap > bestOverlap && overlap > 0) {
                     bestOverlapDirection = "right";
                 }
+
+                if (newBestOverlap > bestOverlap) {
+                    bestOverlapFragmentIndex = i;
+                    bestOverlap = newBestOverlap;
+                }
+
             }
 
             if (bestOverlap > 0) {
@@ -76,16 +133,12 @@ public class Reassembler {
                 }
                 fragments.remove(bestOverlapFragmentIndex);
             } else {
-                noAdd++;
+                /*Fragment has no outer overlap so mark as potential substring of larger fragment */
+                potentialSubstrings++;
             }
         }
 
         return new String(base);
 
     }
-
-    public String getReassembled(){
-        return reassembled;
-    }
-
 }
--- src/test/java/org/edwardsainsbury/reassembler/ReassemblerTest.java
+++ src/test/java/org/edwardsainsbury/reassembler/ReassemblerTest.java
@@ -10,33 +10,52 @@ public class ReassemblerTest {
     @Test
     public void getReassembled() {
 
-        String result = new Reassembler("m quaerat voluptatem.;pora incidunt ut labore et d;, consectetur, " +
+
+        String result = new Reassembler("Hello w;world.").getReassembled();
+        String expectedResult = "Hello world.";
+        assertEquals("Simple test", expectedResult, result);
+
+        result = new Reassembler(" world.;Hello ").getReassembled();
+        assertEquals("Backwards simple test", expectedResult, result);
+
+        result = new Reassembler("").getReassembled();
+        expectedResult = "";
+        assertEquals("Empty string test", result, expectedResult);
+
+        result = new Reassembler("Hello world.").getReassembled();
+        expectedResult = "Hello world.";
+        assertEquals("One fragment test", result, expectedResult);
+
+        result = new Reassembler("Hello world.;").getReassembled();
+        assertEquals("One fragment test inc semi colon", result, expectedResult);
+
+        result = new Reassembler("O draconia;conian devil! Oh la;h lame sa;saint! ").getReassembled();
+        expectedResult = "O draconian devil! Oh lame saint! ";
+        assertEquals("First sample test", expectedResult, result);
+
+        result = new Reassembler("m quaerat voluptatem.;pora incidunt ut labore et d;, consectetur, " +
                 "adipisci velit;olore magnam aliqua;idunt ut labore et dolore magn;uptatem.;i dolorem ipsum qu;iquam" +
                 " quaerat vol;psum quia dolor sit amet, consectetur, a;ia dolor sit amet, conse;squam est, qui " +
                 "do;Neque porro quisquam est, qu;aerat voluptatem.;m eius modi tem;Neque porro qui;, sed quia non " +
                 "numquam ei;lorem ipsum quia dolor sit amet;ctetur, adipisci velit, sed quia non numq;unt ut labore et" +
                 " dolore magnam aliquam qu;dipisci velit, sed quia non numqua;us modi tempora incid;Neque porro" +
                 " quisquam est, qui dolorem i;uam eius modi tem;pora inc;am al").getReassembled();
-        String expectedResult = "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, " +
+        expectedResult = "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, " +
                 "adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam " +
                 "quaerat voluptatem.";
-        assertEquals(expectedResult, result);
+        assertEquals("Second sample test", expectedResult, result);
 
-        result = new Reassembler("O draconia;conian devil! Oh la;h lame sa;saint! ").getReassembled();
-        expectedResult = "O draconian devil! Oh lame saint! ";
-        assertEquals(expectedResult, result);
+    }
 
+    @Test
+    public void main() {
+        String[] emptyArgs = {};
+        Reassembler.main(emptyArgs);
 
-        result = new Reassembler("Hello w;world.").getReassembled();
-        expectedResult = "Hello world.";
-        assertEquals(expectedResult, result);
+        String[] notTestsArgs = {"nottests.txt"};
+        Reassembler.main(notTestsArgs);
 
-        result = new Reassembler(" world.;Hello ").getReassembled();
-        assertEquals(expectedResult, result);
-        /*
-        result = new Reassembler("").getReassembled();
-        expectedResult = "";
-        assertEquals(result, expectedResult);
-        */
+        String[] testsArgs = {"tests.txt"};
+        Reassembler.main(testsArgs);
     }
 }
